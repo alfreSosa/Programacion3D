dbac88abaacf7e922a1d3b0240714054b0a9baaa
Alfredo Sosa <alfres92@gmail.com>
2015-04-21 13:24:06 +0200

    Empezando ejercicio de prueba. No pinta.

--- u-gine3D/src/main.cpp
+++ u-gine3D/src/main.cpp
@@ -9,7 +9,18 @@ int main() {
     Screen::Instance()->Open(800, 600, false);
 
   Ptr<Mesh> caja = ResourceManager::Instance()->LoadMesh("data/box.msh");
+  Ptr<Model> cubo = Model::Create(caja);
+  cubo->GetPosition() = vec3(0,0,0);
+  cubo->GetScale() = vec3(1, 1, 1);
+  Scene::Instance()->AddEntity(cubo.UpCast<Entity>());
+
   Ptr<Camera> mainCamera = Camera::Create();
+  mainCamera->SetUsesTarget(true);
+  mainCamera->GetTarget() = cubo->GetPosition();
+  mainCamera->GetPosition() = vec3(0, 1, 3); 
+  mainCamera->SetColor(vec3(255, 255, 255));
+  mainCamera->SetProjection(glm::perspective<float>(45, (float)Screen::Instance()->GetWidth() / Screen::Instance()->GetHeight(), 1, 1000));
+  mainCamera->SetViewport(0, 0, Screen::Instance()->GetWidth(), Screen::Instance()->GetHeight());
 
   Scene::Instance()->AddEntity(mainCamera.UpCast<Entity>());
 
